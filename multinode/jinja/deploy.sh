88d8187c724e1ef37603afb0e3a24b372b1ff704
Rajesh Mishra <rajeshmishra@apigee.com>
2016-09-14 23:51:57 -0700

    Close to Final Version

--- multinode/jinja/deploy.sh
+++ multinode/jinja/deploy.sh
@@ -1,9 +1,8 @@
 gcloud deployment-manager deployments create $1 --config apigee-vm.yaml
-natIP=$(gcloud compute instances describe $1-apigee-aio --format yaml | grep natIP)
+natIP=$(gcloud compute instances describe $1-apigee-mgmt --format yaml | grep natIP)
 IP=$(echo $natIP | grep -oE "[^:]+$")
 IP="${IP#"${IP%%[![:space:]]*}"}"   # remove leading whitespace characters
 IP="${IP%"${IP##*[![:space:]]}"}"
 echo "Please allow 15 minutes for edge to be installed";
 echo "Please access the management UI at http://$IP:9000";
 echo "Management Server is at http://$IP:8080";
-echo "The API endpoint runs at http://$IP:9001";
