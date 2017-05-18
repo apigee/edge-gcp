fc5484388e21aef917eabf198e6b5b8b83a7d033
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2017-05-17 20:51:47 -0700

    1705 script path changed

--- multinode-autoscale/jinja/deploy.sh
+++ multinode-autoscale/jinja/deploy.sh
@@ -1,4 +1,8 @@
 gcloud deployment-manager deployments create $1 --config apigee-edge.yaml
+red=`tput setaf 1`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+reset=`tput sgr0`
 zone=$(cat apigee-edge.yaml | grep zone)
 zone=$(echo $zone | cut -d':' -f2 | sed -e 's/^[ \t]*//')
 natIP=$(gcloud compute instances describe $1-apigee-mgmt --zone $zone --format yaml | grep natIP)
@@ -13,10 +17,10 @@ devIP="${devIP%"${devIP##*[![:space:]]}"}"
 
 admin_email=$(echo $(cat apigee-edge.yaml | grep ADMIN_EMAIL) | cut -d':' -f2 | sed -e 's/^[ \t]*//' | cut -d' ' -f1 )
 admin_password=$(echo $(cat apigee-edge.yaml | grep APIGEE_ADMINPW) | cut -d':' -f2 | sed -e 's/^[ \t]*//' | cut -d' ' -f1 )
-echo "Please allow 15 minutes for edge to be installed";
-echo "Please access the Edge UI at http://$IP:9000";
-echo "Management Server is at http://$IP:8080";
-echo "Please access the Devportal  at http://$devIP:8079";
-echo "Cred to access EdgeUI/Managament Server/DevPortal is :"$admin_email/$admin_password
-echo "Montitoring Dashboard http://$IP:3000";
-echo "Creds for Monitoring Dashboard admin/admin";
+echo "${red}Please allow 15 minutes for edge to be installed";
+echo "${blue}Please access the Edge UI at ${green}http://$IP:9000";
+echo "${blue}Management Server is at ${green}http://$IP:8080";
+echo "${blue}Please access the Devportal  at ${green}http://$devIP:8079";
+echo "${blue}Cred to access EdgeUI/Managament Server/DevPortal is :${green}"$admin_email/$admin_password
+echo "${blue}Montitoring Dashboard ${green}http://$IP:3000";
+echo "${blue}Creds for Monitoring Dashboard ${green}admin/admin${reset}";
