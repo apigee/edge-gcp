056958df409f160d86531c97275cf77a0fc2ec23
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-02-20 15:38:45 -0800

    topology

--- multinode-autoscale/jinja/ansible-scripts/startup-scripts/prepare_config_file.sh
+++ multinode-autoscale/jinja/ansible-scripts/startup-scripts/prepare_config_file.sh
@@ -65,7 +65,6 @@ sed -i.bak s/DEVPORTAL_ADMIN_USERNAME=/DEVPORTAL_ADMIN_USERNAME="${APIGEE_ADMIN_
 sed -i.bak s/DEVPORTAL_ADMIN_PWD=/DEVPORTAL_ADMIN_PWD="${APIGEE_ADMINPW}"/g dp-config.txt
 sed -i.bak s/DEVPORTAL_ADMIN_EMAIL=/DEVPORTAL_ADMIN_EMAIL="${APIGEE_ADMIN_EMAIL}"/g dp-config.txt
 
-echo "LB_IP_ALIAS ${LB_IP_ALIAS}"
 if [[ -z ${LB_IP_ALIAS} ]]; then
   LB_IP_ALIAS=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google")
 fi
@@ -74,6 +73,7 @@ if [[ $topology == '2' ]]; then
  sed -i.bak s/VHOST_BASEURL=.*//g setup-org-prod.txt
  sed -i.bak s/VHOST_BASEURL=.*//g setup-org-test.txt
 fi
+echo "LB_IP_ALIAS ${LB_IP_ALIAS}"
 
 sed -i.bak s/LBDNS/"${LB_IP_ALIAS}"/g config.txt
 sed -i.bak s/LBDNS/"${LB_IP_ALIAS}"/g setup-org-prod.txt
