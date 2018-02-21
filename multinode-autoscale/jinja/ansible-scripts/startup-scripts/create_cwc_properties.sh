7f8feacfe2244c98b2ceba74ea533a33107c37ce
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-02-21 14:53:31 -0800

    cwc script

--- multinode-autoscale/jinja/ansible-scripts/startup-scripts/create_cwc_properties.sh
+++ multinode-autoscale/jinja/ansible-scripts/startup-scripts/create_cwc_properties.sh
@@ -1,22 +1,18 @@
 #Code with config
-
 property_name=$1
 APP_CWC=$2
-IFS=' '
-
+IFS=','
 mkdir -p /opt/apigee/customer
 mkdir -p /opt/apigee/customer/application
-echo $APP_CWC
-
 app_cwc_ary=($APP_CWC)
-c=1
-for i in "${app_cwc_ary[@]}"
+for item in ${app_cwc_ary[*]}
 do
-if [[ ${i} != 'empty' ]]; then
-  echo ${i}
-  echo "${i}" >> /opt/apigee/customer/application/${property_name}
-  ((c++))
-fi
+    cwc_item=$item
+    cwc_item=${cwc_item/[/}
+    cwc_item=${cwc_item/]/}
+    cwc_item=${cwc_item/\'/}
+    cwc_item=${cwc_item/\'/}
+    cwc_item="$(echo -e "${cwc_item}" | tr -d '[:space:]')"
+    echo $cwc_item >> /opt/apigee/customer/application/${property_name}
 done
 chown apigee:apigee /opt/apigee/customer/application/${property_name}
-IFS=','
