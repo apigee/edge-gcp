9e8eec4f485f5c3031fe946bc3ae32dd04b4a420
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-02-20 16:01:01 -0800

    topology

--- multinode-autoscale/jinja/ansible-scripts/startup-scripts/prepare_config_file.sh
+++ multinode-autoscale/jinja/ansible-scripts/startup-scripts/prepare_config_file.sh
@@ -68,12 +68,16 @@ sed -i.bak s/DEVPORTAL_ADMIN_EMAIL=/DEVPORTAL_ADMIN_EMAIL="${APIGEE_ADMIN_EMAIL}
 if [[ -z ${LB_IP_ALIAS} ]]; then
   LB_IP_ALIAS=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google")
 fi
-if [[ $topology == '2' ]]; then
+
+echo "topology ${topology}"
+
+if [[ $topology = '2' ]]; then
  LB_IP_ALIAS=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google")
  sed -i.bak s/VHOST_BASEURL=.*//g setup-org-prod.txt
  sed -i.bak s/VHOST_BASEURL=.*//g setup-org-test.txt
 fi
 echo "LB_IP_ALIAS ${LB_IP_ALIAS}"
+
 
 sed -i.bak s/LBDNS/"${LB_IP_ALIAS}"/g config.txt
 sed -i.bak s/LBDNS/"${LB_IP_ALIAS}"/g setup-org-prod.txt
