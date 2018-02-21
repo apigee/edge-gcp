88305d6e69b17e8bd4511c1177e49a713235a572
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-02-21 14:07:21 -0800

    create cwc properties

--- multinode-autoscale/jinja/ansible-scripts/startup-scripts/create_cwc_properties.sh
+++ multinode-autoscale/jinja/ansible-scripts/startup-scripts/create_cwc_properties.sh
@@ -1,17 +1,19 @@
 #Code with config
 
-IFS=' '
 property_name=$1
 APP_CWC=$2
+IFS=' '
 
 mkdir -p /opt/apigee/customer
 mkdir -p /opt/apigee/customer/application
+echo $APP_CWC
 
 app_cwc_ary=($APP_CWC)
 c=1
 for i in "${app_cwc_ary[@]}"
 do
 if [[ ${i} != 'empty' ]]; then
+  echo ${i}
   echo "${i}" >> /opt/apigee/customer/application/${property_name}
   ((c++))
 fi
