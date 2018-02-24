3faa1186dec4a48ec74b414ed95436083d48dc87
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-02-23 21:49:28 -0800

    refactor

--- multinode-autoscale/jinja/ansible-scripts/startup-scripts/create_cwc_properties.sh
+++ multinode-autoscale/jinja/ansible-scripts/startup-scripts/create_cwc_properties.sh
@@ -10,11 +10,13 @@ app_cwc_ary=($APP_CWC)
 for item in ${app_cwc_ary[*]}
 do
     cwc_item=$item
-    cwc_item=${cwc_item/[/}
-    cwc_item=${cwc_item/]/}
-    cwc_item=${cwc_item/\'/}
-    cwc_item=${cwc_item/\'/}
-    cwc_item="$(echo -e "${cwc_item}" | tr -d '[:space:]')"
-    echo $cwc_item >> /opt/apigee/customer/application/${property_name}
+    if [["$cwc_item" != "None" ]];then
+      cwc_item=${cwc_item/[/}
+      cwc_item=${cwc_item/]/}
+      cwc_item=${cwc_item/\'/}
+      cwc_item=${cwc_item/\'/}
+      cwc_item="$(echo -e "${cwc_item}" | tr -d '[:space:]')"
+      echo $cwc_item >> /opt/apigee/customer/application/${property_name}
+    fi
 done
 chown apigee:apigee /opt/apigee/customer/application/${property_name}
