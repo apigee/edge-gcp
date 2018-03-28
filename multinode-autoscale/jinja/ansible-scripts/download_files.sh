1c1cec2ad72288afa07b87fd12c058b5753892c0
Rajesh Mishra <rajeshmi@google.com>
2018-03-28 08:35:08 -0700

    download files

--- multinode-autoscale/jinja/ansible-scripts/download_files.sh
+++ multinode-autoscale/jinja/ansible-scripts/download_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+curl -O $SCRIPT_BASEPATH/all_dirs.txt
+curl -O $SCRIPT_BASEPATH/all_files.txt
+
 dirinput="all_dirs.txt"
 while IFS= read -r line
 do
   if [ "$line" != "" ]; then
   		mkdir -p "$line"
+  		#echo $line
   fi
 done < "$dirinput"
 
@@ -12,5 +16,6 @@ while IFS= read -r line
 do
   if [ "$line" != "" ]; then
   		curl $SCRIPT_BASEPATH/$line -o $line
+  		#echo $line
   fi
 done < "$fileinput"
